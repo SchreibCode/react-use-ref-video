f21f1eb58a7f9e37bd877ca48a830ecc6e238670
marvinscheffold <m@marvinscheffold.com>
2022-05-14 16:35:23 +0200

    before video

--- src/components/async.jsx
+++ src/components/async.jsx
@@ -1,21 +1,18 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { requestData } from "../utils/server-mock";
 
 export function Async() {
     const [data, setData] = useState("");
-    const ref = useRef(null);
+
     function onClick() {
         setData("loading...");
         requestData().then((message) => {
-            console.log(ref.current);
-            if (ref.current) {
-                setData(message);
-            }
+            setData(message);
         });
     }
 
     return (
-        <div ref={ref}>
+        <div>
             <button onClick={onClick}>Starte Server Abfrage</button>
             <p>{data}</p>
         </div>
--- src/components/focus.jsx
+++ src/components/focus.jsx
@@ -1,14 +1,7 @@
-import { useEffect, useRef } from "react";
-
 export function Focus() {
-    const ref = useRef(null);
-    useEffect(() => {
-        ref.current.focus();
-    }, []);
-
     return (
         <div>
-            <input ref={ref} placeholder="Dein Name"></input>
+            <input placeholder="Dein Name"></input>
         </div>
     );
 }
